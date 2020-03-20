07135891274dadfa037f52c5774511dff3e2aa38
Joshua Willman <joshua.willman@ymail.com>
2020-03-21 06:45:26 +0800

    Add files via upload
    Updated main script

--- photo_editor.py
+++ photo_editor.py
@@ -1,6 +1,7 @@
 # import necessary modules
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
+from PyQt5.QtWidgets import (QApplication, QMainWindow, QLabel, QAction,
+    QMessageBox, QFileDialog)
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap
 
@@ -27,16 +28,55 @@ class PhotoEditorGUI(QMainWindow):
         self.setWindowTitle("Photo Editor")
         self.showMaximized()
 
+        self.image_label = None
+
+        self.createMenu()
         self.createMainLabel()
 
         self.show()
 
+    def createMenu(self):
+        """Set up the menubar."""
+        open_act = QAction('Open...', self)
+        open_act.setShortcut('Ctrl+O')
+        open_act.triggered.connect(self.openImage)
+
+        exit_act = QAction('Exit', self)
+        exit_act.setShortcut('Ctrl+Q')
+        exit_act.triggered.connect(self.close)
+
+        # Create menubar
+        menu_bar = self.menuBar()
+        menu_bar.setNativeMenuBar(False)
+
+        # Create file menu and add actions
+        file_menu = menu_bar.addMenu('File')
+        file_menu.addAction(open_act)
+        file_menu.addSeparator()
+        file_menu.addAction(exit_act)
+
+        edit_menu = menu_bar.addMenu('Edit')
+        #edit_menu.addAction()
+
     def createMainLabel(self):
         """Create an instance of the imageLabel class and set it 
            as the main window's central widget."""
-        image_label = imageLabel(self)
+        self.image_label = imageLabel(self)
 
-        self.setCentralWidget(image_label)
+        self.setCentralWidget(self.image_label)
+
+    def openImage(self):
+        """ """
+        image_file, _ = QFileDialog.getOpenFileName(self, "Open Image", 
+                "", "JPG Files (*.jpeg *.jpg );;PNG Files (*.png);;Bitmap Files (*.bmp);;\
+                GIF Files (*.gif)")
+        """
+        if image_file:
+            self.image = QPixmap(image_file)
+        """
+
+    def aboutDialog(self):
+        pass
 
     def keyPressEvent(self, event):
         """Handle key press events."""
@@ -48,6 +88,8 @@ class PhotoEditorGUI(QMainWindow):
             else:
                 self.showMaximized()
 
+    def closeEvent(self, event):
+        pass
 
 
 if __name__ == "__main__":
